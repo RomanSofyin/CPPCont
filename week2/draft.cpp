a4d2f68bcc4b4df487ef3a6f28704d826dd41d72
Roman Sofin <roma.sofin@gmail.com>
2019-08-07 21:35:23 +0300

    Create draft.cpp; Add move semantic example

---
+++ week2/draft.cpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "stdafx.h"
+/*
+//Move Semantic example
+
+struct String {
+	String() = default;
+	String(String const & s);
+	String & operator=(String const & s);
+private:
+	char * data_ = nullptr;
+	size_t size_ = 0;
+};
+
+String getCurrentDataString() {
+	String date;
+	// date = "21 oct 2015"
+	return date;
+}
+
+String date = getCurrentDataString();	// redundant copy
+
+*/
+
+
