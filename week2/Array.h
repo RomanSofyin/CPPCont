3eefd14baeaaa17554c82c4f1dbe64ae709e0566
Roman Sofin <roma.sofin@gmail.com>
2019-08-11 16:16:06 +0300

    Implement move constructor and move assignment operator

--- week2/Array.h
+++ week2/Array.h
@@ -20,7 +20,7 @@ struct Array
 	// *RIS: taken from CPPIntro week6
 	Array & operator=(Array const& a) {
 		if (this != &a)
-			Array<T>(oArr).swap(*this);
+			Array<T>(a).swap(*this);
 		return *this;
 	}
 	// *RIS: taken from CPPIntro week6
@@ -37,17 +37,41 @@ struct Array
 	// *RIS: taken from CPPIntro week6
 	T const& operator[](size_t i) const { return *(data_ + i); }
 
-	// реализуйте перемещающий  конструктор
-	//
+	// реализуйте перемещающий конструктор
+	Array(Array && a)		// a - rvalue reference
+	: Array() {				// Since this is a constructor, it must initialize the fields
+							// before moving their values to obj 'a', so to be sure of this
+							// Array() gets called here.
+							// Alternativelly, default values can be assigned for the fields like below:
+							// size_t size_ = 0;
+							// T *    data_ = nullptr;
+	}
+	
 	// реализуйте перемещающий оператор присваивания
+	Array & operator = (Array && a) {
+		clear();			// it's beneficial to free up the resources current object owning
+							// because they are not needed for the obj 'a' but will be assigned to
+							// and may exsist in the programm quite long while the program running
+		swap(a);
+		return *this;
+	}
 
 private:
-	// новая тестовая строка
 	size_t  size_;
 	T *     data_;
 
 	void swap(Array & a) {
 		std::swap(size_, a.size_);
-		std::swap(data_, a.data);
+		std::swap(data_, a.data_);
+	}
+
+	// this clear() works but not sure if this is really good implementation
+	void clear() {
+		for (size_t i = 0; i < size_; i++)
+			data_[i].~T();
+		delete[](char *)data_;
+
+		data_ = nullptr;
+		size_ = 0;
 	}
 };
