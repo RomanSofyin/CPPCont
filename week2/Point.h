aa72cdc9e42fb7bc2fa241e89689f633f405c0ae
Roman Sofin <roma.sofin@gmail.com>
2019-08-24 13:37:01 +0300

    Implement constexpr operators +,-,*

--- week2/Point.h
+++ week2/Point.h
@@ -10,20 +10,23 @@ struct Point
 	double y = 0;
 };
 
+// Note: not completely clear why the parm types must be "const Point &" not just "Point"
+// MVS 2017 compiles it with no problems with "Point", but stepik doesn't acceps it
+
 // operator sum
-constexpr Point & operator+(Point & lp, Point & rp)
+constexpr Point operator+(const Point & lp, const Point & rp)
 {
 	return Point(lp.x + rp.x, lp.y + rp.y);
 }
 
 // subtraction
-constexpr Point & operator-(Point lp, Point  rp)
+constexpr Point operator-(const Point & lp, const Point &  rp)
 {
 	return  Point(lp.x - rp.x, lp.y - rp.y);
 }
 
 // scalar product
-constexpr double operator*(Point lp, Point  rp)
+constexpr double operator*(const Point & lp, const Point &  rp)
 {
 	return (lp.x*rp.x + lp.y*rp.y);
 }
