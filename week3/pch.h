43e65aae3ad0380ad8bdd8c45baf7a91617a5bd9
Roman Sofyin <roman.sofyin@dell.com>
2019-08-29 20:19:39 +0300

    Start implementing max_increasing_len()

--- week3/pch.h
+++ week3/pch.h
@@ -9,5 +9,7 @@
 
 // add headers that you want to pre-compile here
 #include <iostream>
+#include <list>
+#include <algorithm>
 
 #endif //PCH_H
--- week3/week3.cpp
+++ week3/week3.cpp
@@ -3,8 +3,48 @@
 
 #include "pch.h"
 
+// std::list demo
+void listDemo() {
+	using std::string;
+	std::list<string> list = { "One", "Two", "Three" };
+	
+	// iterate over all elements
+	for(string & s : list)
+		std::cout << s << std::endl;
+	
+	// using iterators
+	std::list<string>::iterator i = list.begin();
+	for (; i != list.end(); i++) {	// ++i and i++ have no differences for iterators?
+		if (*i == "Two")
+			break;
+	}
+	list.erase(i);
+}
+
+template <class It>
+size_t max_increasing_len(It p, It q) {
+	size_t res = 0;
+	//std::distance(p, q);
+	for (It i = p, j = p; i != j; i++) {
+		if (*j < *(++p))
+			j = p;
+		else
+			res = std::max(res, std::distance(i, j));
+		int a = 5;
+		a *= 2;
+	}
+
+	return res;
+}
 
 int main()
 {
+	listDemo();
 
+	std::list<int> const l1 = { 7,8,9,4,5,6,1,2,3,4 };
+	size_t len1 = max_increasing_len(l1.begin(), l1.end()); // 4 coresponding to "1,2,3,4"
+	std::list<int> const l2 = { -3,-2,-1,0,0,1,2,3,4,5 };
+	//auto iterator = l2.begin();
+	//iterator.operator->
+	size_t len2 = max_increasing_len(l2.begin(), l2.end()); // 6 coresponding to "0,1,2,3,4,5"
 }
