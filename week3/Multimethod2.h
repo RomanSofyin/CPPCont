41d65eeb80927ed21d48532ae4e83ff6dcc73478
Roman Sofyin <roman.sofyin@dell.com>
2019-09-04 15:08:05 +0300

    Start implementing multimethod

---
+++ week3/Multimethod2.h
@@ -0,0 +1,33 @@
+#pragma once
+
+// Base - base case of the hierarchy
+// Result - type of the value returned by the multimethod
+// Commutative - flag which indicates that the multimethod is commutative (i.e. f(x,y) = f(y,x)).
+template<class Base, class Result, bool Commutative>
+struct Multimethod2
+{
+	// sets implementation of the multimethod
+	// for the types t1 and t2 defined by typeid 
+	// f - function (or functor)
+	// accepting two pointers to Base 
+	// and returning a value with type of Result
+	void addImpl(... t1, ... t2, ... f)
+	{
+
+	}
+
+	// checks wheather implementation of the multimethod exists for the types of objects a and b
+	bool hasImpl(Base* a, Base* b) const
+	{
+		// return true if implementation exists
+		// if opeartion is commutative, presense of implementation b and a should be checked
+		return ...;
+	}
+
+	// Applies multimethod to the objects via the pointers a and b
+	Result call(Base* a, Base* b) const
+	{
+		// returns the result of applying multimethod to a and b
+		return ...;
+	}
+};
---
+++ week3/Shape.h
@@ -0,0 +1,13 @@
+#pragma once
+
+struct Shape { };
+
+struct Rectangle : Shape { };
+
+struct Triangle : Shape { };
+
+// Check if two rectangles intersect
+bool is_intersect_r_r(Shape* a, Shape* b);
+
+// Check if a rectangle and a triangle intersect
+bool is_intersect_r_t(Shape* a, Shape* b);
