8884c3338fc466a301d9502c09227c9b5cc7e168
Roman Sofin <roma.sofin@gmail.com>
2019-10-04 19:04:26 +0300

    Start implementing from_string()

---
+++ week4/bad_from_string.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class bad_from_string : public std::exception {
+	const char * what() const noexcept {
+		return "";
+	}
+};
--- week4/week4.cpp
+++ week4/week4.cpp
@@ -1,7 +1,51 @@
 #include "pch.h"
 #include <iostream>
+#include "bad_from_string.h"
+
+using namespace std;
+
+template<class T>
+T from_string(string const& s)
+{
+	T v;
+	istringstream iss(s);
+	iss >> v;
+	return v;
+}
+
+void from_string_test() {
+	string s1("123");
+	int    a1 = from_string<int>(s1); // a1 = 123
+	double b1 = from_string<double>(s1); // b1 = 123.0
+	string c1 = from_string<string>(s1); // c1 = "123"
+
+	string s2("12.3");
+	try {
+		int    a2 = from_string<int>(s2); // исключение
+	}
+	catch (bad_from_string & e) {
+		cout << "An exception has been thrown by from_string<int>(s2)\n";
+	}
+	double b2 = from_string<double>(s2); // b2 = 12.3
+	string c2 = from_string<string>(s2); // c2 = "12.3"
+
+	string s3("abc");
+	try {
+		int    a3 = from_string<int>(s3); // исключение
+	}
+	catch (bad_from_string & e) {
+		cout << "An exception has been thrown by from_string<int>(s3)\n";
+	}
+	try {
+		double b3 = from_string<double>(s3); // исключение
+	}
+	catch (bad_from_string & e) {
+		cout << "An exception has been thrown by from_string<double>(s3)\n";
+	}
+	string c3 = from_string<string>(s3); // c3 = "abc"
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	from_string_test();
 }
