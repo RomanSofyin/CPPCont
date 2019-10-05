6683236add19042e4d4f437719a59e682d4e8df1
Roman Sofin <roma.sofin@gmail.com>
2019-10-05 12:45:49 +0300

    Implement from_string()

--- week4/week4.cpp
+++ week4/week4.cpp
@@ -8,13 +8,12 @@ T from_string(string const& s)
 {
     T v;
     istringstream iss(s);
-    iss.setf(std::ios_base::skipws);
-    iss >> v;
+    iss >> noskipws >> v;
     if (iss.fail() || iss.peek() != EOF)
         throw bad_from_string(s);
     return v;
 }
-// Safe from string call
+// Safe from_string() call
 #define _SFROM_STRING(type1)                        \
     try {                                           \
         cout << endl << "<" #type1 ">: ";           \
@@ -29,10 +28,10 @@ T from_string(string const& s)
 void from_string_test()
 {    
     vector<string> strings{ "123", "12.3", "", " ", "abc", " 123", "123 ", "12 3", "-1", "a", "a ", " a", "a b", "A" };
-    //               string  +      +                                                                               
-    //               double  +      +                                                                               
-    //                  int  +      e                                                                               
-    //                 char  +      e                                                                               
+    //               string  123    12.3   e    e   "abc"   e       e       e      "-1"  "a"   e     e     e     "A"
+    //               double  123    12.3   e    e    e      e       e       e       -1    e    e     e     e      e 
+    //                  int  123    e      e    e    e      e       e       e       -1    e    e     e     e      e 
+    //                 char  e      e      e   ' '   e      e       e       e       e    'a'   e     e     e     'A' 
     for (auto& str : strings) {
         cout << endl << "from_string(\'" << str << "\'):";
         _SFROM_STRING(string);
