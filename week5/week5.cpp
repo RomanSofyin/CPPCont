45955d2be3f09f17535af1c6baa060c704c46a64
Roman Sofin <roma.sofin@gmail.com>
2019-10-12 21:25:14 +0300

    Keep impl map_reduce()

--- week5/week5.cpp
+++ week5/week5.cpp
@@ -2,12 +2,29 @@
 #include <iostream>
 
 template <class It, class Fun1, class Fun2>
-auto map_reduce(It p, It q, Fun1 f1, Fun2 f2, size_t num) -> decltype(f1(*p)) {
+auto map_reduce(It p, It q, Fun1 f1, Fun2 f2, size_t num) -> decltype(f1) {
     // auto res = f1(*p);
     // while (++p != q)
     //     res = f2(res, f1(*p));
+    
     auto res = f1(*p);
+    
+    std::vector<std::future<decltype(f1(*p))>> v;
+
+
+    for (size_t i = 0; i < num && p != q; i++) {
+        //std::future<int> fut = std::async(std::launch::async, map_reduce_mt, );
+    }
+    
     return res;
+    
+    // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    std::function<int(int)> op = [](int x) {return x+1; };
+    std::future<int> fut = std::async(std::launch::async, op, 5);
+}
+template <class It, class Fun1, class Fun2>
+auto map_reduce_mt(It p, It q, Fun1 f1, Fun2 f2) -> decltype(f1(*p)) {
+
 }
 
 int main()
