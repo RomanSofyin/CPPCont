3ff42f0934d74f4db57f89a6acb39ef031e520e0
Roman Sofin <roma.sofin@gmail.com>
2019-10-19 17:56:59 +0300

    Add Length for IntList

--- week5/week5.cpp
+++ week5/week5.cpp
@@ -106,7 +106,7 @@ struct Fib<0> {
 // определяем список целых чисел
 template <int ... Ns>
 struct IntList;
-// специализация по умолчанию
+// специализация по умолчанию - в списке есть минимум 1 элемент (голова), хвост может быть пустой
 template <int H, int ... T>
 struct IntList<H, T...>
 {
@@ -116,6 +116,19 @@ struct IntList<H, T...>
 // специализация для пустого списка
 template <>
 struct IntList<> { };
+
+// Метафункция Length для вычисления длины списка IntList
+template<typename TL>
+struct Length
+{
+    static int const value = 1 +
+        Length<typename TL::Tail>::value;   // "typename" необходим, чтобы сослаться к члену шаблонного параметра
+};
+template<>
+struct Length<IntList<>>
+{
+    static int const value = 0;
+};
 
 int main()
 {
@@ -160,5 +173,8 @@ int main()
     using primes = IntList<2, 3, 5, 7, 11, 13>;
     constexpr int head = primes::Head;
     using odd_primes = primes::Tail;
+
+    using primes = IntList<2, 3, 5, 7, 11, 13>;
+    constexpr size_t len = Length<primes>::value; // 6
 }
 
